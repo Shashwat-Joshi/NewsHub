17ec1d4a7dde72eec0c54cf8054bb4a5a12adf5c
Shashwat Joshi <shashwat2002joshi@gmail.com>
2023-03-17 12:03:37 +0530

    build changes

--- src/app/core/constants/settings.ts
+++ src/app/core/constants/settings.ts
@@ -1,6 +1,4 @@
 export class AppSettings {
-    public static HOME_HERO_BG_URL : string = "https://images.pexels.com/photos/3601425/pexels-photo-3601425.jpeg?auto=compress&cs=tinysrgb&w=1600";
     public static SIGN_IN_PAGE_IMAGE : string = "https://images.unsplash.com/photo-1615403916271-e2dbc8cf3bf4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
-
     public static PRIMARY_COLOR : string = "#FD4444";
 }
