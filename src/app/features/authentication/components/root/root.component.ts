2558b68d3b7e4f909b11ce3e007887e0df8989b4
Shashwat Joshi <shashwat2002joshi@gmail.com>
2023-03-17 23:45:43 +0530

    finished register UI

--- src/app/features/authentication/components/root/root.component.ts
+++ src/app/features/authentication/components/root/root.component.ts
@@ -1,25 +1,25 @@
 import { Component } from '@angular/core';
-import {AppSettings} from "../../../../core/constants/settings";
-import {NgbCarouselConfig} from "@ng-bootstrap/ng-bootstrap";
+import { AppSettings } from '../../../../core/constants/settings';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'sign-in-page',
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.scss'],
-  providers: [NgbCarouselConfig]
+  providers: [NgbCarouselConfig],
 })
 export class SignInModule {
+  carouselImages: string[] = [
+    AppSettings.SIGN_IN_PAGE_CAROUSEL_URL_1,
+    AppSettings.SIGN_IN_PAGE_CAROUSEL_URL_2,
+    AppSettings.SIGN_IN_PAGE_CAROUSEL_URL_3,
+  ];
+  showSignIn: boolean = true;
+
   constructor(config: NgbCarouselConfig) {
     config.interval = 4000;
     config.keyboard = true;
     config.pauseOnHover = false;
     config.pauseOnFocus = false;
   }
-  carouselImages : string[] = [
-    AppSettings.SIGN_IN_PAGE_CAROUSEL_URL_1,
-    AppSettings.SIGN_IN_PAGE_CAROUSEL_URL_2,
-    AppSettings.SIGN_IN_PAGE_CAROUSEL_URL_3
-  ];
-
-  showSignIn : boolean = false;
 }
