e24ad7bea900c61f75a923c7388296e3e36b5dc7
Shashwat Joshi <shashwat2002joshi@gmail.com>
2023-03-20 02:47:22 +0530

    final

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
   baseUrl: 'https://newsapi.org/v2',
-  // baseUrl: 'http://localhost:3000',
-  authUrl: 'https://ec2-43-205-230-87.ap-south-1.compute.amazonaws.com:8080',
+  authUrl: 'https://83ae-43-205-230-87.in.ngrok.io',
   XApiKey: '75f8bb3e82c746bb9c413ce214a83317',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   baseUrl: 'https://newsapi.org/v2',
-  authUrl: 'https://ec2-43-205-230-87.ap-south-1.compute.amazonaws.com:8080',
+  authUrl: 'https://83ae-43-205-230-87.in.ngrok.io',
   XApiKey: '75f8bb3e82c746bb9c413ce214a83317',
 };
