08769640610ca746a2fcb514b76fd58e2e9ead8a
Shashwat Joshi <shashwat2002joshi@gmail.com>
2023-03-20 00:59:59 +0530

    http and https conflict

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   baseUrl: 'https://newsapi.org/v2',
   // baseUrl: 'http://localhost:3000',
-  authUrl: 'http://ec2-43-205-230-87.ap-south-1.compute.amazonaws.com:8080',
+  authUrl: 'https://ec2-43-205-230-87.ap-south-1.compute.amazonaws.com:8080',
   XApiKey: '75f8bb3e82c746bb9c413ce214a83317',
 };
